bebb68218a7b88ad6d848a4d68c2e088464e4a54
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2021-06-13 22:29:19 -0600

    Add files via upload

---
+++ Inventory Manager/Inventory Manager/InventoryManagerMain.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace Inventory_Manager
+{
+    partial class InventoryManagerMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.lv_Inventory = new System.Windows.Forms.ListView();
+            this.ch_SKU = new System.Windows.Forms.ColumnHeader();
+            this.ch_Name = new System.Windows.Forms.ColumnHeader();
+            this.ch_Cat = new System.Windows.Forms.ColumnHeader();
+            this.ch_Quant = new System.Windows.Forms.ColumnHeader();
+            this.ch_PPrice = new System.Windows.Forms.ColumnHeader();
+            this.ch_LPrice = new System.Windows.Forms.ColumnHeader();
+            this.ch_PDate = new System.Windows.Forms.ColumnHeader();
+            this.btn_Remove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Snap ITC", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(360, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(433, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inventory Manager";
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.BackColor = System.Drawing.Color.LawnGreen;
+            this.btn_Add.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Add.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Add.Location = new System.Drawing.Point(444, 591);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(165, 80);
+            this.btn_Add.TabIndex = 2;
+            this.btn_Add.Text = "Add Item";
+            this.btn_Add.UseVisualStyleBackColor = false;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btn_Search.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Search.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Search.Location = new System.Drawing.Point(814, 591);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(165, 80);
+            this.btn_Search.TabIndex = 3;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = false;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // lv_Inventory
+            // 
+            this.lv_Inventory.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ch_SKU,
+            this.ch_Name,
+            this.ch_Cat,
+            this.ch_Quant,
+            this.ch_PPrice,
+            this.ch_LPrice,
+            this.ch_PDate});
+            this.lv_Inventory.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lv_Inventory.FullRowSelect = true;
+            this.lv_Inventory.GridLines = true;
+            this.lv_Inventory.HideSelection = false;
+            this.lv_Inventory.Location = new System.Drawing.Point(99, 170);
+            this.lv_Inventory.Name = "lv_Inventory";
+            this.lv_Inventory.Size = new System.Drawing.Size(914, 375);
+            this.lv_Inventory.TabIndex = 4;
+            this.lv_Inventory.UseCompatibleStateImageBehavior = false;
+            this.lv_Inventory.View = System.Windows.Forms.View.Details;
+            // 
+            // ch_SKU
+            // 
+            this.ch_SKU.Text = "SKU";
+            this.ch_SKU.Width = 100;
+            // 
+            // ch_Name
+            // 
+            this.ch_Name.Text = "Item Name";
+            this.ch_Name.Width = 150;
+            // 
+            // ch_Cat
+            // 
+            this.ch_Cat.Text = "Catagory";
+            this.ch_Cat.Width = 150;
+            // 
+            // ch_Quant
+            // 
+            this.ch_Quant.Text = "Stock";
+            // 
+            // ch_PPrice
+            // 
+            this.ch_PPrice.Text = "Purchase Price";
+            this.ch_PPrice.Width = 150;
+            // 
+            // ch_LPrice
+            // 
+            this.ch_LPrice.Text = "List Price";
+            this.ch_LPrice.Width = 150;
+            // 
+            // ch_PDate
+            // 
+            this.ch_PDate.Text = "Purchase Date";
+            this.ch_PDate.Width = 150;
+            // 
+            // btn_Remove
+            // 
+            this.btn_Remove.BackColor = System.Drawing.Color.Fuchsia;
+            this.btn_Remove.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Remove.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Remove.Location = new System.Drawing.Point(628, 591);
+            this.btn_Remove.Name = "btn_Remove";
+            this.btn_Remove.Size = new System.Drawing.Size(165, 80);
+            this.btn_Remove.TabIndex = 5;
+            this.btn_Remove.Text = "Remove Item";
+            this.btn_Remove.UseVisualStyleBackColor = false;
+            this.btn_Remove.Click += new System.EventHandler(this.btn_Remove_Click);
+            // 
+            // InventoryManagerMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1128, 841);
+            this.Controls.Add(this.btn_Remove);
+            this.Controls.Add(this.lv_Inventory);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Name = "InventoryManagerMain";
+            this.Text = "Add Item";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.ListView lv_Inventory;
+        private System.Windows.Forms.ColumnHeader ch_SKU;
+        private System.Windows.Forms.ColumnHeader ch_Name;
+        private System.Windows.Forms.ColumnHeader ch_Cat;
+        private System.Windows.Forms.ColumnHeader ch_Quant;
+        private System.Windows.Forms.ColumnHeader ch_PPrice;
+        private System.Windows.Forms.ColumnHeader ch_LPrice;
+        private System.Windows.Forms.ColumnHeader ch_PDate;
+        private System.Windows.Forms.Button btn_Remove;
+    }
+}
+
---
+++ Inventory Manager/Inventory Manager/InventoryManagerMain.cs
@@ -0,0 +1,103 @@
+﻿using InventoryItemClass;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_Manager
+{
+    public partial class InventoryManagerMain : Form
+    {
+        public static List<InventoryItemClass.InventoryItem> list;
+
+
+        public InventoryManagerMain()
+        {
+            //This list will store InventoryItem objects from the InventoryItemClass I have created
+            list = new List<InventoryItemClass.InventoryItem>();
+
+            InitializeComponent();
+
+
+            //I have added these instances of InventoryItem class for test purposes so we can test all functionality without
+            //having to add in items manually. This will not be in the final project.
+
+            InventoryItem i1 = new InventoryItem("1", "Shirt1", "Polo1", 10, 6.00, 8.00, "Hi", DateTime.Today);
+            list.Add(i1);
+            InventoryItem i2 = new InventoryItem("2", "Shirt2", "Polo2", 12, 6.00, 8.00, "Hi", DateTime.Today);
+            list.Add(i2);
+            InventoryItem i3 = new InventoryItem("3", "Shirt3", "Polo3", 15, 6.00, 8.00, "Hi", DateTime.Today);
+            list.Add(i3);
+
+            PopulateInventory(list);
+        }
+
+        /*
+         * This button click event for the add button launches the add item form which allows the user to input the item 
+         * details. I have used the f.ShowDialog() so that the next line of code will not execute until the user adds the item.
+         * After user adds the the item to inventory, the inventory list will update.
+         */
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            
+            f_Add f = new f_Add();
+            f.ShowDialog();
+
+            PopulateInventory(list);
+
+        }      
+
+       
+        /*
+         * This method removes the selected item in the listview from the List<> list and then updates the inventory list effectivly
+         * removing the selected item from the entire application.
+         */
+        private void btn_Remove_Click(object sender, EventArgs e)
+        {
+            list.RemoveAt(lv_Inventory.SelectedIndices.Count);
+
+            PopulateInventory(list);
+          
+
+        }
+
+       
+        /*
+         * This method updates the lv_Inventory listview with all items found in the list. It is used for many situations like adding,
+         * removing, and initial population of the items in the list to the listview lv_Inventory
+         */
+        private void PopulateInventory(List<InventoryItem> list)
+        {
+            lv_Inventory.Items.Clear();
+            int itemCnt = 0;
+            foreach (InventoryItem iI in list)
+            {
+
+                ListViewItem lvi = new ListViewItem(list[itemCnt].item_SKU);
+                lvi.SubItems.Add(list[itemCnt].item_Type);
+                lvi.SubItems.Add(list[itemCnt].item_Name);
+                lvi.SubItems.Add(list[itemCnt].item_Quantity.ToString());
+                lvi.SubItems.Add(list[itemCnt].item_Purchase_Price.ToString("C", CultureInfo.CurrentCulture));
+                lvi.SubItems.Add(list[itemCnt].item_List_Price.ToString("C", CultureInfo.CurrentCulture));
+                lvi.SubItems.Add(list[itemCnt].item_List_Date.ToString("d"));
+
+                lv_Inventory.Items.Add(lvi);
+                itemCnt++;
+
+            }
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            SearchResult searchResult = new SearchResult();
+            searchResult.ShowDialog();
+
+        }
+    }
+}
